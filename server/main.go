aff95dc6ce3a9b0ca37d1edd8fae0e37b8b85d5b
JacobPuff <jacob@todandlorna.com>
2022-03-20 19:57:10 -0600

    feat: add error handling and smarter logging

--- server/main.go
+++ server/main.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net"
 	"net/http"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 
 	// This import path is based on the name declaration in the go.mod,
 	// and the gen/proto/go output location in the buf.gen.yaml.
 	searchv1 "github.com/JacobPuff/github-search-api/gen/proto/go/githubsearch/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const RESULTS_PER_PAGE = "30"
@@ -28,26 +32,25 @@ type GithubResults struct {
 }
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-	}
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+	run()
 }
 
-func run() error {
+func run() {
 	listenOn := "0.0.0.0:8080"
 	listener, err := net.Listen("tcp", listenOn)
 	if err != nil {
-		return fmt.Errorf("failed to listen on %s: %w", listenOn, err)
+		log.Fatalf("failed to listen on %s: %w", listenOn, err)
 	}
 
 	server := grpc.NewServer()
 	searchv1.RegisterGithubSearchServiceServer(server, &GithubSearchServiceServer{})
-	log.Println("Listening on", listenOn)
+	log.Infof("Listening on %s", listenOn)
 	if err := server.Serve(listener); err != nil {
-		return fmt.Errorf("failed to serve gRPC server: %w", err)
+		log.Fatalf("failed to serve gRPC server: %w", err)
 	}
-
-	return nil
 }
 
 type GithubSearchServiceServer struct {
@@ -57,16 +60,20 @@ type GithubSearchServiceServer struct {
 func (s *GithubSearchServiceServer) Search(ctx context.Context, req *searchv1.SearchRequest) (*searchv1.SearchResponse, error) {
 	search_term := req.GetSearchTerm()
 	user := req.GetUser()
-	log.Printf("Got a request to search for '%t' filtered to user '%t'", search_term, user)
+	if search_term == "" && user == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "Need at least one of search_term or user")
+	}
+	log.Infof("Got a request to search for '%t' filtered to user '%t'", search_term, user)
 	githubRequest, err := http.NewRequest("GET", "https://api.github.com/search/code", nil)
 	if err != nil {
-		log.Printf("An error occured when creating request")
+		log.Error("An error occured when creating request")
+		return nil, status.Errorf(codes.Internal, "An error occured while processing your request")
 	}
 
 	githubRequest.Header.Add("Accept", "application/vnd.github.v3+json")
 	queryParams := githubRequest.URL.Query()
 	if user != "" {
-		log.Println("USER SET")
+		log.Debug("USER SET")
 		search_term = fmt.Sprintf("%s user:%s", search_term, user)
 	}
 
@@ -76,16 +83,31 @@ func (s *GithubSearchServiceServer) Search(ctx context.Context, req *searchv1.Se
 
 	response, err := http.DefaultClient.Do(githubRequest)
 	if err != nil {
-		log.Printf("An error occured when sending request to github api %s", err)
+		log.Errorf("An error occured when sending request to github api %s", err)
+		return nil, status.Errorf(codes.Internal, "An error occured while processing your request")
 	}
 	defer response.Body.Close()
-	log.Printf("URL: %s", githubRequest.URL)
-	log.Printf("STATUS: %s", response.Status)
+	log.Debugf("URL: %s", githubRequest.URL)
+	log.Debugf("STATUS: %s", response.Status)
+
+	if response.StatusCode != http.StatusOK {
+		log.Errorf("Received bad request status: %d", response.StatusCode)
+		if response.StatusCode == http.StatusForbidden {
+			return nil, status.Errorf(codes.Unauthenticated, "You aren't able to access this resource")
+		}
+		if response.StatusCode == http.StatusUnprocessableEntity {
+			return nil, status.Errorf(codes.InvalidArgument, "Search term is invalid, scope needs to be narrowed down")
+		}
+		if response.StatusCode == http.StatusServiceUnavailable {
+			return nil, status.Errorf(codes.Unavailable, "Service Unavailable")
+		}
+	}
 
 	githubResults := &GithubResults{}
 	err = json.NewDecoder(response.Body).Decode(githubResults)
 	if err != nil {
-		log.Printf("An error occured while decoding json %s", err)
+		log.Errorf("An error occured while decoding json %s", err)
+		return nil, status.Errorf(codes.Internal, "An error occured while processing your request")
 	}
 	grpcResults := []*searchv1.Result{}
 	for _, item := range githubResults.Items {
@@ -95,6 +117,8 @@ func (s *GithubSearchServiceServer) Search(ctx context.Context, req *searchv1.Se
 		}
 		grpcResults = append(grpcResults, result)
 	}
-	log.Println("\n")
+	// This makes the output easier to read
+	fmt.Println("\n")
+
 	return &searchv1.SearchResponse{Results: grpcResults}, nil
 }
