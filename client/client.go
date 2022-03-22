4221b9947ce94480121007a774199c634b528a26
JacobPuff <jacob@todandlorna.com>
2022-03-22 12:22:01 -0600

    fix: use appconfig to get env vars and setup logging

--- client/client.go
+++ client/client.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 
+	appconfig "github.com/JacobPuff/github-search-api/appconfig"
 	// This import path is based on the name declaration in the go.mod,
 	// and the gen/proto/go output location in the buf.gen.yaml.
 	searchv1 "github.com/JacobPuff/github-search-api/gen/proto/go/githubsearch/v1"
@@ -13,18 +13,11 @@ import (
 	"google.golang.org/grpc"
 )
 
-func setupLogging() {
-	log.SetFormatter(&log.JSONFormatter{})
-	log.SetOutput(os.Stdout)
-	log.SetLevel(log.InfoLevel)
-}
-
 func main() {
-	setupLogging()
-	var SearchTerm = GetEnvOrDefault("SEARCH_TERM", "search repo:jacobpuff/github-search-api")
-	var User = GetEnvOrDefault("USER", "")
-	connectTo := GetEnvOrDefault("GH_SEARCH_SERVER_ADDRESS", "0.0.0.0")
-	connectTo += ":" + GetEnvOrDefault("GH_SEARCH_SERVER_PORT", "9090")
+	appconfig.SetupLogging()
+	var SearchTerm = appconfig.GetEnvOrDefault("SEARCH_TERM", "search repo:jacobpuff/github-search-api")
+	var User = appconfig.GetEnvOrDefault("USER", "")
+	connectTo := appconfig.GHSearchServerAddress + ":" + appconfig.GHSearchServerPort
 
 	log.Infof("Attempting connection to %s", connectTo)
 	conn, err := grpc.Dial(connectTo, grpc.WithBlock(), grpc.WithInsecure())
@@ -46,12 +39,4 @@ func main() {
 	for _, item := range response.Results {
 		log.Infof("file_url: %s\nrepo: %s\n\n", item.FileUrl, item.Repo)
 	}
-}
-
-func GetEnvOrDefault(env, defaultValue string) string {
-	value := os.Getenv(env)
-	if value != "" {
-		return value
-	}
-	return defaultValue
 }
